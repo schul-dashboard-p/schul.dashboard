43d3288ebfdb86953b9435af1b6e4e62ff639de5
schul-dashboard-p <huberpatrick1994@gmail.com>
2026-09-08 20:06:58 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://dfvlpwsplgiqoeqtyrrj.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_t8OeoXHa4s8Ct8gO9Cr_fw_LWen8nzE'
+};
---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='school-dashboard-v3';
+const STATIC=['./','./index.html','./manifest.json','./config.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC))));
+self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch', e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
+});
